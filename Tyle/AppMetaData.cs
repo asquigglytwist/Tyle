225bef4ea2de770e281f7b0b0b05a5eeae7dbf79
asquigglytwist <9925933+asquigglytwist@users.noreply.github.com>
2020-04-25 23:02:05 +0530

    The modified version of AppMetaData, with new fields and version string format.

--- Tyle/AppMetaData.cs
+++ Tyle/AppMetaData.cs
@@ -2,21 +2,35 @@ namespace Tyle
 {
     public static class AppMetaData
     {
+        public const string BuildTimeStamp = "2020-04-25  17:24:05";
+#region AssemblyInfo
         public const string ApplicationName = "Tyle";
         public const string Description = "Tail Your Logs Efficiently...";
         public const string CompanyName = "ASquigglyTwist";
+#if DEBUG
+        public const string AssemblyConfiguration = "Debug";
+#else
+        public const string AssemblyConfiguration = "Release";
+#endif
         public const string CopyRightYear = "2020";
         public const string CopyRight = "Copyright © " + CompanyName + ", " + CopyRightYear + "; All rights reserved.";
         public const string TradeMark = ApplicationName + ", " + CompanyName;
         // [BIB]:  https://stackoverflow.com/questions/64602/what-are-differences-between-assemblyversion-assemblyfileversion-and-assemblyin
         private const string major = "0";
-        private const string minor = "1";
-        private const string build = "200424";
-        private const string revision = "140620";
+        private const string minor = "2004";
+        private const string build = "25";
+        private const string revision = "1724";
         public const string ProductVersion = major + "." + minor + ".0.0";
-        public const string AssemblyVersion = major + "." + minor + "." + build + "." + revision;
+        public const string AssemblyVersion = major + "." + minor + "." + build + ".0";
         // [BIB]:  https://stackoverflow.com/questions/17144355/how-can-i-replace-every-occurrence-of-a-string-in-a-file-with-powershell
         // [BIB]:  https://stackoverflow.com/questions/2249619/how-to-format-a-datetime-in-powershell
-        public const string AssemblyFileVersion = major + "." + minor + "." + "200424.140620";
+        public const string AssemblyFileVersion = major + "." + minor + "." + build + "." + revision;
+#endregion //  AssemblyInfo
+#region Git - Repo Details
+        // [BIB]:  https://stackoverflow.com/questions/6245570/how-to-get-the-current-branch-name-in-git
+        public const string RepoBranch = "master";
+        // [BIB]:  https://stackoverflow.com/questions/5694389/get-the-short-git-version-hash
+        public const string CommitHash = "b973c086537fa3ca497a01c3afa52ed27e8761c7";
+#endregion //  Git - Repo Details
     }
 }
