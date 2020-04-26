eed4f21a34a3c25676d577a228d4c80c8057fa47
asquigglytwist <9925933+asquigglytwist@users.noreply.github.com>
2020-04-26 23:47:02 +0530

    Adding DisplayFont field to HighlightConfig.

--- Src/Core/Code/HighlightsConfig.cs
+++ Src/Core/Code/HighlightsConfig.cs
@@ -53,9 +53,12 @@ namespace Tyle.Core
         public readonly string Pattern;
         public readonly bool IgnoreCase, Bold, Italic, Underline, Strikeout;
         public readonly Color ForeGround, BackGround;
+        public readonly Font DisplayFont;
 
-        public HighlightConfig(string pattern, bool ignoreCase, bool bold, bool italic,
-            bool underline, bool strikeout, Color foreGround, Color backGround)
+        public HighlightConfig(string pattern, bool ignoreCase,
+            bool bold, bool italic, bool underline, bool strikeout,
+            Color foreGround, Color backGround,
+            Font displayFont)
         {
             Pattern = pattern;
             IgnoreCase = ignoreCase;
@@ -65,6 +68,13 @@ namespace Tyle.Core
             Strikeout = strikeout;
             ForeGround = foreGround;
             BackGround = backGround;
+            FontStyle style = FontStyle.Regular
+                | (bold ? FontStyle.Bold : FontStyle.Regular)
+                | (italic ? FontStyle.Italic : FontStyle.Regular)
+                | (underline ? FontStyle.Underline : FontStyle.Regular)
+                | (strikeout ? FontStyle.Strikeout : FontStyle.Regular);
+            //DisplayFont = new Font("Verdana", 14, style);
+            DisplayFont = new Font(displayFont, style);
         }
     }
 }
