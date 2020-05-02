5ed48c2a38385666b9a240e3463d75de29f30bc9
asquigglytwist <9925933+asquigglytwist@users.noreply.github.com>
2020-05-03 00:15:37 +0530

    Adding a ListView control to display the AppMetaData information.

--- Src/UI/WinForms/UI/AboutBox.Designer.cs
+++ Src/UI/WinForms/UI/AboutBox.Designer.cs
@@ -28,14 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lsvAboutDetails = new System.Windows.Forms.ListView();
+            this.lsvColValue = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lsvColField = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
+            // 
+            // lsvAboutDetails
+            // 
+            this.lsvAboutDetails.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.lsvColValue,
+            this.lsvColField});
+            this.lsvAboutDetails.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lsvAboutDetails.GridLines = true;
+            this.lsvAboutDetails.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
+            this.lsvAboutDetails.HideSelection = false;
+            this.lsvAboutDetails.Location = new System.Drawing.Point(0, 226);
+            this.lsvAboutDetails.MultiSelect = false;
+            this.lsvAboutDetails.Name = "lsvAboutDetails";
+            this.lsvAboutDetails.ShowItemToolTips = true;
+            this.lsvAboutDetails.Size = new System.Drawing.Size(720, 348);
+            this.lsvAboutDetails.TabIndex = 0;
+            this.lsvAboutDetails.UseCompatibleStateImageBehavior = false;
+            // 
+            // lsvColValue
+            // 
+            this.lsvColValue.DisplayIndex = 1;
+            this.lsvColValue.Text = "";
+            // 
+            // lsvColField
+            // 
+            this.lsvColField.DisplayIndex = 0;
+            this.lsvColField.Text = "";
             // 
             // AboutBox
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(640, 459);
+            this.ClientSize = new System.Drawing.Size(720, 574);
+            this.Controls.Add(this.lsvAboutDetails);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "AboutBox";
@@ -47,5 +79,9 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView lsvAboutDetails;
+        private System.Windows.Forms.ColumnHeader lsvColValue;
+        private System.Windows.Forms.ColumnHeader lsvColField;
     }
 }
