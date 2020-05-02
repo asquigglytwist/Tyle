90787dbb2cc31f449859b3bb6271b346ffb73a48
asquigglytwist <9925933+asquigglytwist@users.noreply.github.com>
2020-05-03 00:29:31 +0530

    Overcoming the limit of 260 chars in a ListViewColumn by making the control OwnerDrawn.

--- Src/UI/WinForms/UI/TailViewerForm.Designer.cs
+++ Src/UI/WinForms/UI/TailViewerForm.Designer.cs
@@ -1,4 +1,7 @@
-﻿using System.Drawing;
+﻿using Core.Utils;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace Tyle.UI
 {
@@ -46,12 +49,13 @@ namespace Tyle.UI
             // 
             // mnuTailForm
             // 
+            this.mnuTailForm.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
             this.mnuTailForm.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.mnuTailForm.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.mnuEdit});
             this.mnuTailForm.Location = new System.Drawing.Point(0, 0);
             this.mnuTailForm.Name = "mnuTailForm";
-            this.mnuTailForm.Size = new System.Drawing.Size(282, 28);
+            this.mnuTailForm.Size = new System.Drawing.Size(282, 33);
             this.mnuTailForm.TabIndex = 1;
             this.mnuTailForm.Text = "mnuTail";
             this.mnuTailForm.Visible = false;
@@ -66,27 +70,27 @@ namespace Tyle.UI
             this.mnuEdit.MergeAction = System.Windows.Forms.MergeAction.Insert;
             this.mnuEdit.MergeIndex = 1;
             this.mnuEdit.Name = "mnuEdit";
-            this.mnuEdit.Size = new System.Drawing.Size(47, 24);
+            this.mnuEdit.Size = new System.Drawing.Size(58, 29);
             this.mnuEdit.Text = "&Edit";
             // 
             // mnuECopy
             // 
             this.mnuECopy.Name = "mnuECopy";
             this.mnuECopy.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.C)));
-            this.mnuECopy.Size = new System.Drawing.Size(181, 26);
+            this.mnuECopy.Size = new System.Drawing.Size(270, 34);
             this.mnuECopy.Text = "&Copy";
             this.mnuECopy.Click += new System.EventHandler(this.mnuECopy_Click);
             // 
             // tssAfterCopy
             // 
             this.tssAfterCopy.Name = "tssAfterCopy";
-            this.tssAfterCopy.Size = new System.Drawing.Size(178, 6);
+            this.tssAfterCopy.Size = new System.Drawing.Size(267, 6);
             // 
             // mnuEFind
             // 
             this.mnuEFind.Name = "mnuEFind";
             this.mnuEFind.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.F)));
-            this.mnuEFind.Size = new System.Drawing.Size(181, 26);
+            this.mnuEFind.Size = new System.Drawing.Size(270, 34);
             this.mnuEFind.Text = "&Find";
             this.mnuEFind.Click += new System.EventHandler(this.mnuEFind_Click);
             // 
@@ -95,7 +99,7 @@ namespace Tyle.UI
             this.mnuEFindNext.Enabled = false;
             this.mnuEFindNext.Name = "mnuEFindNext";
             this.mnuEFindNext.ShortcutKeys = System.Windows.Forms.Keys.F3;
-            this.mnuEFindNext.Size = new System.Drawing.Size(181, 26);
+            this.mnuEFindNext.Size = new System.Drawing.Size(270, 34);
             this.mnuEFindNext.Text = "Find &Next";
             this.mnuEFindNext.Click += new System.EventHandler(this.mnuEFindNext_Click);
             // 
@@ -103,16 +107,16 @@ namespace Tyle.UI
             // 
             this.lsvTailViewer.BackColor = System.Drawing.Color.WhiteSmoke;
             this.lsvTailViewer.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.lsvTailViewer.Font = new System.Drawing.Font("Courier New", 12F);
             this.lsvTailViewer.ForeColor = System.Drawing.Color.Black;
             this.lsvTailViewer.FullRowSelect = true;
             this.lsvTailViewer.GridLines = true;
             this.lsvTailViewer.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
-            this.lsvTailViewer.Location = new System.Drawing.Point(0, 28);
+            this.lsvTailViewer.HideSelection = false;
+            this.lsvTailViewer.Location = new System.Drawing.Point(0, 33);
             this.lsvTailViewer.Name = "lsvTailViewer";
             this.lsvTailViewer.ShowGroups = false;
             this.lsvTailViewer.ShowItemToolTips = true;
-            this.lsvTailViewer.Size = new System.Drawing.Size(282, 225);
+            this.lsvTailViewer.Size = new System.Drawing.Size(282, 220);
             this.lsvTailViewer.TabIndex = 0;
             this.lsvTailViewer.UseCompatibleStateImageBehavior = false;
             this.lsvTailViewer.View = System.Windows.Forms.View.Details;
@@ -121,7 +125,7 @@ namespace Tyle.UI
             // 
             // TailViewerForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(282, 253);
             this.Controls.Add(this.lsvTailViewer);
@@ -151,6 +155,8 @@ namespace Tyle.UI
     #region TailListView
     class TailListView : System.Windows.Forms.ListView
     {
+        const TextFormatFlags TFFlags = TextFormatFlags.Left | TextFormatFlags.ExpandTabs | TextFormatFlags.SingleLine | TextFormatFlags.NoPrefix;
+
         public TailListView() : base()
         {
             DoubleBuffered = true;
@@ -158,20 +164,38 @@ namespace Tyle.UI
             View = System.Windows.Forms.View.Details;
             FullRowSelect = true;
             Dock = System.Windows.Forms.DockStyle.Fill;
-            Font = new Font("Courier New", 12, FontStyle.Regular);
             ShowGroups = false;
             ShowItemToolTips = true;
             GridLines = true;
             HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
             Columns.Add("LineNumber", -1, System.Windows.Forms.HorizontalAlignment.Left);
             Columns.Add("Line", -1, System.Windows.Forms.HorizontalAlignment.Left);
+            LabelWrap = true;
+            ShowGroups = false;
+            OwnerDraw = true;
+            DrawSubItem += TailListView_DrawSubItem;
         }
-        public void AutoFitColumnsToContent(string longestLine)
+
+        private void TailListView_DrawSubItem(object sender, DrawListViewSubItemEventArgs e)
+        {
+            if (e.Item.Selected)
+            {
+                e.SubItem.BackColor = SystemColors.Highlight;
+                e.SubItem.ForeColor = SystemColors.HighlightText;
+            }
+            // Draw the standard header background.
+            e.DrawBackground();
+            e.DrawFocusRectangle(e.Bounds);
+            System.Windows.Forms.TextRenderer.DrawText(e.Graphics, e.SubItem.Text, Font, e.Bounds, e.SubItem.ForeColor, TFFlags);
+        }
+
+        public void AutoFitColumnsToContent(string longestLine, Font currentFont)
         {
             int longLineMaxWidth = (int)System.Math.Ceiling(MeasureString(longestLine).Width + 50f);
             Columns[0].Width = -2;
             Columns[1].Width = (Columns[1].Width < longLineMaxWidth ? longLineMaxWidth : Columns[1].Width);
         }
+
         public void SelectVirtualItem(int index)
         {
             SelectedIndices.Clear();
@@ -183,6 +207,7 @@ namespace Tyle.UI
             }
             Focus();
         }
+
         protected SizeF MeasureString(string stringToMeasure)
         {
             // [BIB]:  https://stackoverflow.com/a/6705023
@@ -193,6 +218,7 @@ namespace Tyle.UI
             }
             return measuredSize;
         }
+
         public int SearchBeginIndex
         {
             get
