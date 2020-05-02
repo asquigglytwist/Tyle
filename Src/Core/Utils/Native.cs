ded1a63f2c610a71010008b3246208198208c6bb
asquigglytwist <9925933+asquigglytwist@users.noreply.github.com>
2020-05-03 00:23:52 +0530

    Adding a class for supporting Native methods (using DllImports).

---
+++ Src/Core/Utils/Native.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Core.Utils
+{
+    public static class Native
+    {
+        public const int LVM_FIRST     =  0x1000;
+        public const int EM_LIMITTEXT  =  0xC5;
+
+        [DllImport("user32.dll", CharSet = CharSet.Ansi)]
+        public static extern IntPtr SendMessage(IntPtr hWnd, int msg, int len, IntPtr order);
+    }
+}
