f477205e754f7ea5d8c30c0edea4a09870f499e3
asquigglytwist <9925933+asquigglytwist@users.noreply.github.com>
2020-05-03 21:43:23 +0530

    Updated AppMetaData.

---
+++ Src/Core/Code/HighlightsHandler.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Code
+{
+    public static class HighlightsHandler
+    {
+        static HighlightsHandler()
+        {
+            AllConfigs = new List<HighlightConfig>();
+        }
+
+        public static void Add(HighlightConfig config)
+        {
+            lock (AllConfigs)
+            {
+                AllConfigs.Add(config);
+            }
+        }
+
+        public static void Remove(string uniqueID)
+            => Remove(AllConfigs.FindIndex(x => x.UniqueID.Equals(uniqueID)));
+
+        public static void Remove(int index)
+        {
+            lock (AllConfigs)
+            {
+                var count = AllConfigs.Count;
+                if (count > 0 && index > 0 && count < index)
+                {
+                    AllConfigs.RemoveAt(index);
+                }
+            }
+            var exMsg = $"Index {index} out of range for {nameof(AllConfigs)} with {AllConfigs.Count} items.";
+            throw new IndexOutOfRangeException(exMsg);
+        }
+
+        public static HighlightConfig? TryGetConfigFor(string line)
+        {
+            var temp = AllConfigs.FindIndex(
+                c => line.IndexOf(c.Pattern, StringComparison.CurrentCultureIgnoreCase) > -1);
+            if (temp >= 0)
+            {
+                return AllConfigs[temp];
+            }
+            return null;
+        }
+
+        public static void UpdateConfigs(List<HighlightConfig> newConfigs)
+        {
+            AllConfigs = newConfigs;
+        }
+
+        public static List<HighlightConfig> AllConfigs
+        { get; private set; }
+    }
+}
