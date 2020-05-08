161bd669a14d22636fb05eaa441014c805f71450
asquigglytwist <9925933+asquigglytwist@users.noreply.github.com>
2020-05-08 23:18:40 +0530

    Issue #15 - Config persistence between app launches
    Saving the Configs to a JSON file, using NewtonSoft.JSON.

--- Src/Core/Code/HighlightsHandler.cs
+++ Src/Core/Code/HighlightsHandler.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace Core.Code
@@ -50,6 +52,8 @@ namespace Core.Code
         public static void UpdateConfigs(List<HighlightConfig> newConfigs)
         {
             AllConfigs = newConfigs;
+            var configsInJSON = JsonConvert.SerializeObject(newConfigs, Formatting.Indented);
+            File.WriteAllText("Tyle.prefs", configsInJSON);
         }
 
         public static List<HighlightConfig> AllConfigs
