38aa5ba9d4030416522a6066d451a568d5c6a08a
asquigglytwist <9925933+asquigglytwist@users.noreply.github.com>
2020-05-16 19:51:09 +0530

    Moving the LogEntry class to a separate file.

---
+++ Src/Core/Code/LogEntry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Code
+{
+    public class LogEntry
+    {
+        public LogEntry(string line)
+        {
+            Line = line;
+        }
+
+        public string Line
+        { get; private set; }
+    }
+}
