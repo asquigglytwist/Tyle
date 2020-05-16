27d8d6fb5f28cea7c33eaa1f6760a37c8f412e29
asquigglytwist <9925933+asquigglytwist@users.noreply.github.com>
2020-05-16 20:28:26 +0530

    Adding IConfig abstract class which will be inherited by specific type of configurations.

---
+++ Src/Core/Code/IConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Code
+{
+    public enum ConfigAction
+    {
+        Highlight,
+        Filter,
+        React
+    }
+
+    public abstract class IConfig
+    {
+        public readonly string UniqueID;
+        public readonly string Pattern;
+        public readonly bool IgnoreCase;
+        public readonly bool IsRegex;
+        public readonly ConfigAction Action;
+
+        public IConfig(string pattern)
+        {
+            UniqueID = Guid.NewGuid().ToString("N").ToLowerInvariant();
+            Pattern = pattern;
+        }
+
+        public abstract bool DoesLineMatch(string line);
+    }
+}
