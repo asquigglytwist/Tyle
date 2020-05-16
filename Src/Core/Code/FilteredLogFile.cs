d9df85525fa497b5bd0c9bff76cb801c443ed4ba
asquigglytwist <9925933+asquigglytwist@users.noreply.github.com>
2020-05-16 19:53:06 +0530

    Creating a FilteredLogFile for a LogFile with entries that are filtered.

---
+++ Src/Core/Code/FilteredLogFile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Code
+{
+    public class FilteredLogFile : LogFileStream
+    {
+        List<LogEntry> filteredLogEntries;
+
+        public FilteredLogFile(string filePath)
+            : base(filePath)
+        {
+            filteredLogEntries = new List<LogEntry>();
+        }
+    }
+}
