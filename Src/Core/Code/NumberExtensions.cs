79f3b3c1bf7b01f3cef59e431ce3ed186a3159c2
asquigglytwist <9925933+asquigglytwist@users.noreply.github.com>
2020-05-16 19:52:32 +0530

    Adding NumberExtensions for convenience.

---
+++ Src/Core/Code/NumberExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Code
+{
+    public static class NumberExtensions
+    {
+        // [BIB]:  https://stackoverflow.com/questions/281640/how-do-i-get-a-human-readable-file-size-in-bytes-abbreviation-using-net
+        public static string AsReadableFileSize(this long byteCount)
+        {
+            string[] suf = { "B", "KB", "MB", "GB", "TB", "PB", "EB" }; //Longs run out around EB
+            if (byteCount < 1)
+            {
+                return "0 B";// + suf[0];
+            }
+            long bytes = Math.Abs(byteCount);
+            int place = Convert.ToInt32(Math.Floor(Math.Log(bytes, 1024)));
+            double num = Math.Round(bytes / Math.Pow(1024, place), 1);
+            return $"{(Math.Sign(byteCount) * num)} {suf[place]}";
+        }
+    }
+}
