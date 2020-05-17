24e2e45726a96ad219c0bcbdad4f113a0edd3f79
asquigglytwist <9925933+asquigglytwist@users.noreply.github.com>
2020-05-17 16:22:15 +0530

    Making HighlightConfig as an independent class (and not a child of IConfig)

--- Src/Core/Code/HighlightsConfig.cs
+++ Src/Core/Code/HighlightsConfig.cs
@@ -7,17 +7,15 @@ using System.Threading.Tasks;
 
 namespace Core.Code
 {
-    public class HighlightConfig : IConfig
+    public class HighlightConfig
     {
         public readonly bool Bold, Italic, Underline, Strikeout;
         public readonly Color ForeGround, BackGround;
         public readonly Font DisplayFont;
 
-        public HighlightConfig(string pattern, bool ignoreCase, bool isRegex,
-            bool bold, bool italic, bool underline, bool strikeout,
+        public HighlightConfig(bool bold, bool italic, bool underline, bool strikeout,
             Color foreGround, Color backGround,
             Font displayFont)
-            : base(ConfigAction.Highlight, pattern, ignoreCase, isRegex)
         {
             Bold = bold;
             Italic = italic;
