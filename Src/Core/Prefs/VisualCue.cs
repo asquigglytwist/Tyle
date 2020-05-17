3ccf1620e2b0a76a1f1c38d2356cbaa0a196d667
asquigglytwist <9925933+asquigglytwist@users.noreply.github.com>
2020-05-17 17:37:09 +0530

    Documenting VisualCue (adding comments)

--- Src/Core/Prefs/VisualCue.cs
+++ Src/Core/Prefs/VisualCue.cs
@@ -7,19 +7,53 @@ using System.Threading.Tasks;
 
 namespace Core.Code
 {
+    #region VisualCue
     public class VisualCue
     {
+        #region Fields
+        /// <summary>
+        /// Bold-style for the text
+        /// </summary>
         public readonly bool Bold;
+        /// <summary>
+        /// Italicizes the text
+        /// </summary>
         public readonly bool Italic;
+        /// <summary>
+        /// Underlines the text
+        /// </summary>
         public readonly bool Underline;
+        /// <summary>
+        /// Strike-out the text
+        /// </summary>
         public readonly bool Strikeout;
+        /// <summary>
+        /// The text color to be used
+        /// </summary>
         public readonly Color ForeGround;
+        /// <summary>
+        /// Background color for the text
+        /// </summary>
         public readonly Color BackGround;
+        /// <summary>
+        /// Font to be used for displaying the text
+        /// </summary>
         public readonly Font DisplayFont;
+        #endregion // Fields
 
+        #region Constructor
+        /// <summary>
+        /// Creates a new instance of <see cref="VisualCue"/>
+        /// </summary>
+        /// <param name="bold">Bold style for the text</param>
+        /// <param name="italic">Italicizes the text</param>
+        /// <param name="underline">Underlines the text</param>
+        /// <param name="strikeout">Strike-out the text</param>
+        /// <param name="foreGround">Foreground color for the text</param>
+        /// <param name="backGround">Background color for the text</param>
+        /// <param name="displayFont">Font to be used for displaying the text</param>
         public VisualCue(bool bold, bool italic, bool underline, bool strikeout,
-            Color foreGround, Color backGround,
-            Font displayFont)
+            Color foreGround, Color backGround, Font displayFont)
         {
             Bold = bold;
             Italic = italic;
@@ -34,5 +68,7 @@ namespace Core.Code
                 | (strikeout ? FontStyle.Strikeout : FontStyle.Regular);
             DisplayFont = new Font(displayFont, style);
         }
+        #endregion // Constructor
     }
+    #endregion // VisualCue
 }
