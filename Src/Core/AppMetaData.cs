ddd13150c207e89822fbd617809885aac7effcce
asquigglytwist <9925933+asquigglytwist@users.noreply.github.com>
2020-05-19 08:11:35 +0530

    Updating the AppMetaData; Pushing to trigger build.

--- Src/Core/AppMetaData.cs
+++ Src/Core/AppMetaData.cs
@@ -2,7 +2,7 @@ namespace Tyle
 {
     public static class AppMetaData
     {
-        public const string BuildTimeStamp = "2020-05-17  18:29:00 UTC";
+        public const string BuildTimeStamp = "2020-05-19  02:10:29 UTC";
         public const string BuildMode = "WinForms";
         public const double PrefsVersion = 1.0;
 #region AssemblyInfo
@@ -20,8 +20,8 @@ namespace Tyle
         // [BIB]:  https://stackoverflow.com/questions/64602/what-are-differences-between-assemblyversion-assemblyfileversion-and-assemblyin
         private const string major = "0";
         private const string minor = "2005";
-        private const string build = "17";
-        private const string revision = "1829";
+        private const string build = "19";
+        private const string revision = "0210";
         public const string ProductVersion = major + "." + minor + ".0.0";
         public const string AssemblyVersion = major + "." + minor + "." + build + ".0";
         // [BIB]:  https://stackoverflow.com/questions/17144355/how-can-i-replace-every-occurrence-of-a-string-in-a-file-with-powershell
@@ -32,7 +32,7 @@ namespace Tyle
         // [BIB]:  https://stackoverflow.com/questions/6245570/how-to-get-the-current-branch-name-in-git
         public const string RepoBranch = "master";
         // [BIB]:  https://stackoverflow.com/questions/5694389/get-the-short-git-version-hash
-        public const string CommitHash = "10efeb70d02c57d40a800db28c73526d01ab026c";
+        public const string CommitHash = "6d34186a556ca0cc1c98c985277e5e6c10c49145";
 #endregion //  Git - Repo Details
     }
 }
