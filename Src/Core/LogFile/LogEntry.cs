696e04b460bf614c7a502fcf39d20213f510fd3c
asquigglytwist <9925933+asquigglytwist@users.noreply.github.com>
2020-05-19 07:48:50 +0530

    Making LogEntry hold a reference to the first matching Rule.

--- Src/Core/LogFile/LogEntry.cs
+++ Src/Core/LogFile/LogEntry.cs
@@ -2,16 +2,20 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Core.Code
+namespace Core.Prefs
 {
     public class LogEntry
     {
         public LogEntry(string line)
         {
             Line = line;
+            MatchingRule = null;
         }
 
         public string Line
         { get; private set; }
+
+        public Rule MatchingRule
+        { get; set; }
     }
 }
