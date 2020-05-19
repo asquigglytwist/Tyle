c2e16f3a9fa705d8dabd1345ed78c28ac5a5bf38
asquigglytwist <9925933+asquigglytwist@users.noreply.github.com>
2020-05-19 07:41:08 +0530

    Adding RulesEngine.

---
+++ Src/Core/Prefs/RulesEngine.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Core.Prefs
+{
+    public static class RulesEngine
+    {
+        #region Properties
+        /// <summary>
+        /// <see cref="List{T}"/> of all <see cref="Rule"/>(s)
+        /// </summary>
+        public static List<Rule> Rules
+        { get; set; }
+        #endregion // Properties
+
+        public static VisualCue TryGetDecorationFor(LogEntry entry)
+        {
+            return Rules.Find(c => c.DoesLineMatch(entry.Line))?.Decoration;
+        }
+
+        public static List<LogEntry> UpdateLogEntries(List<LogEntry> logEntries)
+        {
+            var filteredLogEntries = new List<LogEntry>();
+            lock (Rules)
+            {
+                var itrRules = new Queue<Rule>(Rules);
+                for (int i = 0; i < logEntries.Count; i++)
+                {
+                    var entry = logEntries[i];
+                    var match = Rules.Find(r => r.DoesLineMatch(entry.Line));
+                    if (match != null)
+                    {
+                        if (match.IsAnExcludeFilter)
+                        {
+                            continue;
+                        }
+                        entry.MatchingRule = match;
+                    }
+                    filteredLogEntries.Add(entry);
+                }
+            }
+            return filteredLogEntries;
+        }
+    }
+}
