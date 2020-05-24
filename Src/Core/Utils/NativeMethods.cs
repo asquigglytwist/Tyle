af247aff17d14a2be63fe0bc522f0f3ee792400a
asquigglytwist <9925933+asquigglytwist@users.noreply.github.com>
2020-05-24 13:20:25 +0530

    Removing unused members from NativeMethods.

--- Src/Core/Utils/NativeMethods.cs
+++ Src/Core/Utils/NativeMethods.cs
@@ -7,9 +7,6 @@ namespace Core.Utils
 {
     public static class NativeMethods
     {
-        public const int LVM_FIRST     =  0x1000;
-        public const int EM_LIMITTEXT  =  0xC5;
-
         [DllImport("user32.dll", CharSet = CharSet.Ansi)]
         internal static extern IntPtr SendMessage(IntPtr hWnd, uint msg, uint len, IntPtr order);
     }
