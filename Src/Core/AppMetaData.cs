abdc9a71f49add57f2f66101b69644a7ced9aba4
asquigglytwist <9925933+asquigglytwist@users.noreply.github.com>
2020-05-24 15:10:54 +0530

    Updating AppMetaData & Template.

--- Src/Core/AppMetaData.cs
+++ Src/Core/AppMetaData.cs
@@ -2,7 +2,7 @@ namespace Tyle
 {
     public static class AppMetaData
     {
-        public const string BuildTimeStamp = "2020-05-24  07:19:22 UTC";
+        public const string BuildTimeStamp = "2020-05-24  09:40:08 UTC";
         public const string BuildMode = "WinForms";
         public const double PrefsVersion = 0.1;
 #region AssemblyInfo
@@ -23,7 +23,7 @@ namespace Tyle
         private const string major = "0";
         private const string minor = "2005";
         private const string build = "24";
-        private const string revision = "0719";
+        private const string revision = "0940";
         public const string ProductVersion = major + "." + minor + ".0.0";
         public const string AssemblyVersion = major + "." + minor + "." + build + ".0";
         // [BIB]:  https://stackoverflow.com/questions/17144355/how-can-i-replace-every-occurrence-of-a-string-in-a-file-with-powershell
@@ -34,7 +34,7 @@ namespace Tyle
         // [BIB]:  https://stackoverflow.com/questions/6245570/how-to-get-the-current-branch-name-in-git
         public const string RepoBranch = "master";
         // [BIB]:  https://stackoverflow.com/questions/5694389/get-the-short-git-version-hash
-        public const string CommitHash = "7b3493dbce7fc6d29f31b703e5e9b76e74f4c573";
+        public const string CommitHash = "dfe4d25c3dfd1e046fefe5ac1ef3304998af6ffb";
 #endregion //  Git - Repo Details
     }
 }
