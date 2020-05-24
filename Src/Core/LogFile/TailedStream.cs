62e9636d6fa02ce95e92dad630ef95444a39f66f
asquigglytwist <9925933+asquigglytwist@users.noreply.github.com>
2020-05-24 19:31:25 +0530

    Adding (a new implementation of) TailedStream.

---
+++ Src/Core/LogFile/TailedStream.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Core.LogFile
+{
+    public class TailedStream : LogFileStream
+    {
+        FileSystemWatcher fileWatcher;
+        public delegate void TailedFileChangedHandler(object sender, TailedFileChangedEventArgs e);
+        public event TailedFileChangedHandler TailedFileChangedEventHandler;
+
+        public TailedStream(string filePath) : base(filePath)
+        {
+        }
+    }
+}
