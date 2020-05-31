42237b7e13e81e17a175ec8960d2bafb63bd79e9
asquigglytwist <9925933+asquigglytwist@users.noreply.github.com>
2020-05-31 20:24:49 +0530

    Adding LogStreamFactory to handle new stream creation.

---
+++ Src/Core/LogFile/LogStreamFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.LogFile
+{
+    public static class LogStreamFactory
+    {
+        public static LogFileStream GetStreamFor(string filePath,
+            TailedStream.TailedFileChangedHandler tailedFileChanged = null)
+        {
+            if (tailedFileChanged == null)
+            {
+                return new LogFileStream(filePath);
+            }
+            return new TailedStream(filePath, tailedFileChanged);
+        }
+    }
+}
